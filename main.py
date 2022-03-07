62fd8421324ac0896feee37e2650dac4fb724a93
Daniel <danielglennsmith92@gmail.com>
2022-03-07 09:10:25 +0000

    Initialised globals, added provider/consumer roles

--- main.py
+++ main.py
@@ -51,10 +51,44 @@ debug = args.debug
 # Initialise variables that are later changed in if statements
 client = False
 translator = False
+CoAP_Enable = False
+CoAP_Consume = False
+CoAP_Provide = False
+CoAP_Observe = False
+MQTT_Enable = False
+MQTT_Consume = False
+MQTT_Provide = False
+AMQP_Enable = False
+AMQP_Consume = False
+AMQP_Provide = False
+HTTP_Enable = False
+HTTPS_Enable = False
+HTTPS_Only = False
+HTTP_Consume = False
+HTTP_Provide = False
+MQTT_brokerAddress = ""
+AMQP_exchangeAddress = ""
+
 
 if not (args.configFile or exists(defaultConfig)):
     print("-- No config specified and no default found. Using built in default --")
     # TODO set default values
+    CoAP_Enable = True
+    CoAP_Consume = False
+    CoAP_Provide = True
+    CoAP_Observe = False
+    MQTT_Enable = True
+    MQTT_Consume = True
+    MQTT_Provide = False
+    AMQP_Enable = False
+    AMQP_Consume = False
+    AMQP_Provide = False
+    HTTP_Enable = False
+    HTTPS_Enable = False
+    HTTPS_Only = False
+    HTTP_Consume = False
+    HTTP_Provide = False
+
 else:
     if args.configFile:
         if debug:
@@ -68,6 +102,9 @@ else:
 
     # TODO get values from config file in use - Finish me
     config = json.load(configFile)
+    if config['debugPrint']:
+        debug = True
+        print('debug enabled by config file')
     role = config['role']
     translatorAddress = config['translatorAddress']
     if config['protocols']:
@@ -75,32 +112,52 @@ else:
             if protocol['name'] == 'CoAP':
                 if protocol['enabled']:
                     CoAP_Enable = True
-                else:
-                    CoAP_Enable = False
-                if protocol.get('options', False).get('observeSupport', False):
-                    CoAP_Observe = True
-                else:
-                    CoAP_Observe = False
-                if protocol.get('options', False).get('optional', False):  # POC that options can be optional
-                    pass
-                else:
-                    print('Test')
+                    if protocol.get('options', False).get('observeSupport', False):
+                        CoAP_Observe = True
+                    if protocol.get('options', False).get('consume', False):
+                        CoAP_Consume = True
+                    if protocol.get('options', False).get('provide', False):
+                        CoAP_Provide = True
             elif protocol['name'] == 'MQTT':
                 if protocol['enabled']:
                     MQTT_Enable = True
+                    if protocol.get('options', False).get('brokerAddress', False):
+                        MQTT_brokerAddress = protocol.get('options').get('brokerAddress')
+                        if debug:
+                            print("MQTT Broker address set to {}".format(MQTT_brokerAddress))
+                    else:
+                        print('--WARNING MQTT is enabled but no broker address is specified')
+                    if protocol.get('options', False).get('consume', False):
+                        MQTT_Consume = True
+                    if protocol.get('options', False).get('provide', False):
+                        MQTT_Provide = True
             elif protocol['name'] == 'AMQP':
                 if protocol['enabled']:
                     AMQP_Enable = True
+                    if protocol.get('options', False).get('exchangeAddress', False):
+                        AMQP_exchangeAddress = protocol.get('options').get('exchangeAddress')
+                        if debug:
+                            print("AMQP Exchange address set to {}".format(AMQP_exchangeAddress))
+                    else:
+                        print('--WARNING AMQP is enabled but no exchange address is specified')
+                    if protocol.get('options', False).get('consume', False):
+                        AMQP_Consume = True
+                    if protocol.get('options', False).get('provide', False):
+                        AMQP_Provide = True
             elif protocol['name'] == 'HTTP':
                 if protocol['enabled']:
                     HTTP_Enable = True
-                if protocol.get('options', False).get('HTTPS', False):
-                    HTTPS_Enable = True
-                if protocol.get('options', False).get('HTTPSOnly', False):
-                    HTTPS_Only = True
+                    if protocol.get('options', False).get('HTTPS', False):
+                        HTTPS_Enable = True
+                    if protocol.get('options', False).get('HTTPSOnly', False):
+                        HTTPS_Only = True
+                    if protocol.get('options', False).get('consume', False):
+                        HTTP_Consume = True
+                    if protocol.get('options', False).get('provide', False):
+                        HTTP_Provide = True
             else:
-                print('--WARNING\tInvalid Protocol name found in config file. Skipping this protocol. If this is '
-                      'unexpected check the configuration and consider restarting the program.')
+                print('--WARNING\tInvalid Protocol name "{}" found in config file. Skipping this protocol. If this is '
+                      'unexpected check the configuration and consider restarting the program.'.format(protocol['name']))
     if role == "Client":
         client = True
     elif role == "Translator":
